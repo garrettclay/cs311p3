36d4dec8909d113f4c70acccf3dc5e3db8339f09
Garrett Clay <clayg@onid.orst.edu>
2013-02-15 10:51:17 -0800

    now making pipes outside of while loop making processes. working on getting parser as separate function so it can deliver words round robin style

--- uniqify.c
+++ uniqify.c
@@ -14,6 +14,50 @@ int **pipe_generator(int pipe_cnt){
 	return pipe_array;
 }
 
+void make_pipes(int **pipe_array, int num_sort){
+ 	/* pipe from parser to sort(s) */
+	for(int j = 0; j < num_sort; j++){
+		if(pipe(pipe_array[j]) != 0){
+			perror("pipe");
+			exit(-1);
+		}
+	}
+}
+
+void parser(int **par_sort_pipe, int num_sort){
+
+	int j = num_sort;
+
+	/* bind STDOUT to pipe write end */
+	if(par_sort_pipe[j][1] != STDOUT_FILENO){
+		if(dup2(par_sort_pipe[j][1], STDOUT_FILENO) == -1){
+			perror("dup2 2");
+		}
+		if(close(par_sort_pipe[j][1]) == -1){
+			perror("close 5");
+		}
+	}
+	FILE *output[num_sort];
+	output[j] = fdopen(STDOUT_FILENO, "w");
+
+	char word_in[50];
+	int scan_result = 1;
+	while(scan_result != EOF){
+		// To do: scanf w/o including chars but 
+		// so you can stil read EOF
+		// scanf("%[^!-@]", word_in);
+		scan_result = scanf("%s", word_in);
+		fputs(word_in, output[j]);
+		fputc('\n', output[j]);
+	}
+	/* closing all streams */
+	for(int l = 0; l<num_sort; l++){
+		fclose(output[l]);	
+	}
+
+
+}
+
 void suppressor(int **input_pipe){
 
 
@@ -35,29 +79,33 @@ void solder_pipes_shut(int **open_pipe, int num_pipes){
 
 int main(int argc, char **argv){
 
-	char buf[50] = "why does FPUT work?\n";
-	char buf2[50] = "this prints fine.\n";
-	char buf3[50];
+	//char buf[50] = "why does FPUT work?\n";
+	//char buf2[50] = "this prints fine.\n";
+	//char buf3[50];
 	int result;
 	int status;
 	int j = 0;
 	int k = 0;
 	pid_t child;
 	int num_sort = argc;
+
 	//delete following line for more than one process
 	num_sort = 1;
 
 	// pass argc in here eventually
-	int **prs_fds = pipe_generator(1);
-	int **spr_fds = pipe_generator(1);
+	int **prs_fds = pipe_generator(num_sort);
+	int **spr_fds = pipe_generator(num_sort);
 
+	make_pipes(prs_fds, num_sort);
+	make_pipes(spr_fds, num_sort);
 
-//	pipe_array[1][0] = 5;
-	while(j < 1){
-		if(pipe(prs_fds[j]) != 0){
-			perror("pipe");
-			exit(-1);
-		}
+	while(j < num_sort){
+	//	/* pipe from parser to sort(s) */
+	//	if(pipe(prs_fds[j]) != 0){
+	//		perror("pipe");
+	//		exit(-1);
+	//	}
+		/* pipe from sort(s) to suppressor */
 	//	if(pipe(spr_fds[k]) != 0){
 	//		perror("pipe");
 	//		exit(-1);
@@ -71,20 +119,20 @@ int main(int argc, char **argv){
 				break;
 
 			case 0:
-				//child case
-				//read from prs_fds
-				//write to sup_fds 
+				//child case 
 			//	close(STDIN_FILENO);
 			//	close(STDOUT_FILENO);
+				/* close write end of parser pipe */
 				if(close(prs_fds[j][1]) == -1){	//write pipe end
 					perror("close 1");
 				}
+				/* close read end of suppressor pipe */
 	//			if(close(spr_fds[k][0]) == -1){
 	//				perror("close 7");
 	//			}				
 
 
-				// duplicate stdout
+				/* bind sorts STDIN to output of parser pipe */
 				if(prs_fds[j][0] != STDIN_FILENO){
 					if(dup2(prs_fds[j][0], STDIN_FILENO) == -1){
 						perror("dup2 1");
@@ -93,6 +141,7 @@ int main(int argc, char **argv){
 						perror("close 2");
 					}	
 				}	
+				/* bind sorts STDOUT to input of sup pipe */
 	/*			if(spr_fds[k][1] != STDOUT_FILENO){
 					if(dup2(spr_fds[k][1], STDOUT_FILENO) ==-1){
 						perror("dup3 1");
@@ -105,27 +154,22 @@ int main(int argc, char **argv){
 
 			//	FILE *input = fdopen(STDIN_FILENO, "r");
 
-	/*			int o = 0;
-				while(o < 8){
-					fgets(buf, 12, input);	
-					printf("%s\n",buf);
-					o++;
-				}		
-	*/			
 	//			close(spr_fds[k][1]);
-	//
-				solder_pipes_shut(prs_fds, 1);
+	
+				solder_pipes_shut(prs_fds, num_sort);
+			//	solder_pipes_shut(spr_fds, num_sort);
 				//close(prs_fds[j][0]);
 				execlp("sort", "sort", (char *)NULL);
 				break;		
 	
 			default:
 				//parent case -- result holds pid of child
-				//close output end of pipe		
+				/* close read end of pipe */		
 				if(close(prs_fds[j][0]) == -1){
 					perror("close 3");
 				}
-				//bind stdout to file input
+				
+				/* bind STDOUT to pipe write end */
 				if(prs_fds[j][1] != STDOUT_FILENO){
 					if(dup2(prs_fds[j][1], STDOUT_FILENO) == -1){
 						perror("dup2 2");
@@ -134,31 +178,38 @@ int main(int argc, char **argv){
 						perror("close 5");
 					}
 				}
-				FILE *output = fdopen(STDOUT_FILENO, "w");
+				FILE *output[num_sort];
+				output[j] = fdopen(STDOUT_FILENO, "w");
 
 				char word_in[50];
 				int scan_result = 1;
 				while(scan_result != EOF){
-					// To do: scanf without including chars but 
+					// To do: scanf w/o including chars but 
 					// so you can stil read EOF
 					// scanf("%[^!-@]", word_in);
 					scan_result = scanf("%s", word_in);
-					fputs(word_in, output);
-					fputc('\n', output);
+					fputs(word_in, output[j]);
+					fputc('\n', output[j]);
 				}
-			
-				fclose(output);	
+				// closing all streams 
+				for(int l = 0; l<num_sort; l++){
+					fclose(output[l]);	
+				}
+
 				//child = wait(&status);
 				break; 
 		}
 		j++;
 	}
 
+//	parser(prs_fds, num_sort);
+
 	//if(close(prs_fds[1]) == -1){
 	//	perror("close 4");
 	//}
 	
 	child = wait(&status);
-	printf("Current PID: %d\n",getpid());
+	free(prs_fds);
+	free(spr_fds);
 	return 0;
 }
